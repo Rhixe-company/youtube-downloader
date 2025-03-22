8e953b9f8c5b27392b231d6dc8ef530caf93a41e
rhixecompany <rhixecompany@gmail.com>
2025-03-22 20:23:05 +0100

    second commit

---
+++ main_noplaylist.py
@@ -0,0 +1,39 @@
+from __future__ import unicode_literals
+
+import logging
+
+from yt_dlp import YoutubeDL
+
+logger = logging.getLogger(__name__)
+
+
+def main(input_url):
+    yt_opts = {
+        "verbose": False,
+        "format": "best/bestvideo+bestaudio",
+        "writeautomaticsub": True,
+        "subtitlesformat": "vtt",
+        "skip_download": False,
+        "outtmpl": "downloads/%(title)s.%(ext)s",
+        "subtitleslangs": ["en"],
+        "writethumbnail": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegVideoConvertor",
+                "preferedformat": "mp4",
+            },
+        ],
+    }
+    msg = f"Starting {input_url}"
+    logger.info(msg)
+    with YoutubeDL(yt_opts) as ydl:
+        ydl.download([input_url])
+    msg1 = f"Done Downloading {input_url}"
+    logger.info(msg1)
+
+
+if __name__ == "__main__":
+    save_path = "downloads"
+    input_url = input("Enter your URL: ")
+    # url = "https://youtube.com/playlist?list=PL-2EBeDYMIbTLulc9FSoAXhbmXpLq2l5t&si=gF6wqVQf4vtWZ5d0"
+    main(input_url)
