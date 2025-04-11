76c72873df8d8ad86b559e450f91832f44283e22
rhixecompany <rhixecompany@gmail.com>
2025-04-11 23:18:34 +0100

    Updates

--- main_loop_noplaylist.py
+++ main_loop_noplaylist.py
@@ -10,7 +10,8 @@ logger = logging.getLogger(__name__)
 def main(input_url):
     yt_opts = {
         "verbose": False,
-        "format": "bv+ba",
+        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
+        # "format": "bv+ba",
         "writeautomaticsub": True,
         "subtitlesformat": "vtt",
         "skip_download": False,
@@ -20,34 +21,26 @@ def main(input_url):
         "postprocessors": [
             {
                 "key": "FFmpegVideoConvertor",
-                "preferedformat": "mkv",
+                "preferedformat": "mp4",
             },
         ],
     }
-    msg = f"Starting {input_url}"
-    logger.debug(msg)
+    msg1 = f"Downloading {input_url}"
+    logger.debug(msg1)
     with YoutubeDL(yt_opts) as ydl:
         ydl.download([input_url])
-    msg1 = f"Done Downloading {input_url}"
-    logger.debug(msg1)
 
 
 if __name__ == "__main__":
     urls = [
-        "https://youtu.be/4VvYOWiQch4?si=S0G5-0PVMBZbAN4Z",
-        "https://youtu.be/_q6QlaBakTM?si=1FY4oluHsPind7wB",
-        "https://youtu.be/JyIuMooTRFA?si=LS0Zx14aHy23yGzC",
-        "https://youtu.be/CpvkCzd2O6A?si=aeAfGbnBLaOg1Tg-",
-        "https://youtu.be/PZKH5S0C8EI?si=oOpqchS4Ys8A-nIR",
-        "https://youtu.be/Wp6LRijW9wg?si=Amv9_9Wm8_KwPMsK",
-        "https://youtu.be/m5dwVl4-GAs?si=iDY7cqNOke71R6yc",
-        "https://youtu.be/vBdBB7kM74w?si=Em7CtOVew46v68t4",
-        "https://youtu.be/YQuyZLOXDW4?si=3DmikIRKyv6V7u0t",
-        "https://youtu.be/UCkRBhfU6zo?si=jc0F3OrnG62cz5RF",
-        "https://youtu.be/SDiaKMD1hPA?si=Arv6UW6jLjeB04q5",
-        "https://youtu.be/HhCNYt_RWl8?si=ArZxhK5MMQHwxQBL",
+        # "https://youtu.be/ZJr7mmaEXiE?si=qhAXq4E50yhysvrh",
+        "https://youtu.be/beVGYjMSRRk?si=6dxG4tqL49t1j7fK",
+        "https://youtu.be/lc1sOvRaFpg?si=IVvhHtii21ZNlZA_",
+        "https://youtu.be/PgHaH8tGdWw?si=Z4xPiwMl6QO1PN4Z",
+        "https://youtu.be/Pc96-4_gqJ4?si=fVywee_x83PyCZba",
     ]
     for link in urls:
         url = link
         main(url)
-        logger.debug(f"Done Downloading {url}")
+    logger.debug(f"Done Downloading {urls}")
+    # yt-dlp -Uv -N 5 --progress -f bestvideo+bestaudio/best --download-archive archives/archive-97ac0bc8.txt --write-subs --convert-subs srt --merge-output-format=mkv -i --add-metadata --write-annotations --write-info-json --write-thumbnail --write-description -o %(uploader)s-97ac0bc8/%(upload_date)s__%(id)s.%(ext)s --yes-playlist https://www.youtube.com/c/NBNNNewsLaz/videos
